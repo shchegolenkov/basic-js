5bc9968098ce8c77413f6ef991c296aafb0eccd9
shchegolenkov <denis.amorales@ya.ru>
2023-04-17 18:08:09 +0300

    feat: sum-digits done

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let arr = String(n).split("");
+  let maxArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      maxArr.push(+(arr[i] + arr[j]));
+    }
+  }
+  console.log(maxArr);
+  return Math.max(...maxArr);
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n = 0) {
+  let sum = 0;
+  n.toString()
+    .split("")
+    .forEach((digit) => (sum += +digit));
+  console.log("sum before compare", sum);
+  if (sum.toString().length === 1) {
+    return sum;
+  } else {
+    console.log("sum", sum);
+    return getSumOfDigits(sum);
+  }
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
